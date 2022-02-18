ba4ee17dcc4a7ec53d7800967c545566b19a0842
roiding <dingran@ran-ding.ga>
2022-02-18 14:53:56 +0800

    保持启动脚本不死

--- start.sh
+++ start.sh
@@ -2,11 +2,20 @@ if [ $AUTHORIZATION ]
 then
 
     python ./shuafen.py $AUTHORIZATION >/logs/shuafen.log
-
+    pid1=$!
 fi
 if [ $SENDKEY ]
 then
 
     python ./jiankong.py $SENDKEY >/logs/jiankong.log
-
+    pid2=$!
 fi
+while true
+do
+    if [[(-n "${pid1}" && -d "/proc/${pid1}") ||(-n "${pid2}" && -d "/proc/${pid2}")]]
+    then
+        sleep 1s
+    else
+        break
+    fi
+done
